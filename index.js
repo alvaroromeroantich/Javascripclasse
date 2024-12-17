d0f9980f2a7d033231282b1b98842f10c7b4a239
alvaroromero <alvaroromero@alumnat.iesdamiahuguet.net>
2024-12-17 12:25:46 +0100

    Comit inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello World";
+    document.getElementById("demo").style.color = "red";
+}
+function concatena(){
+    let Nom = document.getElementById("CampNom").value;
+    let Llinatges =document.getElementById("CampLlinatges").value;
+    alert("Hola " + Nom + " " + Llinatges)
+}
